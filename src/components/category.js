2ef0d3dc4d99e579ce78b215fe3cd6d8c18b905b
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-05 12:19:57 +0300

    category css added

--- src/components/category.js
+++ src/components/category.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './Styles/Category.css'
+import './Styles/Category.css';
 
 function Category() {
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
   const { category } = useParams();
 
   useEffect(() => {
@@ -16,6 +17,14 @@ function Category() {
     fetchProducts();
   }, [category]);
 
+  const handlePlayClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleCloseClick = () => {
+    setSelectedProduct(null);
+  };
+
   return (
     <section className="category">
       <div className="container">
@@ -26,9 +35,22 @@ function Category() {
               <img src={product.image} alt={product.title} className="category__product-image" />
               <h3 className="category__product-title">{product.title}</h3>
               <p className="category__product-price">${product.price}</p>
+              <button className="category__product-button" onClick={() => handlePlayClick(product)}>Play</button>
             </div>
           ))}
         </div>
+        {selectedProduct && (
+          <div className="category__modal">
+            <div className="category__modal-content">
+              <button className="category__modal-close" onClick={handleCloseClick}>X</button>
+              <img src={selectedProduct.image} alt={selectedProduct.title} className="category__modal-image" />
+              <div className="category__modal-details">
+                <h3 className="category__modal-title">{selectedProduct.title}</h3>
+                <p className="category__modal-description">{selectedProduct.description}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
