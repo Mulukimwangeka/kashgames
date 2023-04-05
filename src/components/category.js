ec1602a866814160baf1aa6a79900da3deff1335
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-05 12:01:24 +0300

    category css added

--- src/components/category.js
+++ src/components/category.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './Styles/Category.css'
 
 function Category() {
   const [products, setProducts] = useState([]);
