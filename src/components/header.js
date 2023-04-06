de4143bffc49e053390168a4163f2cc42aaafc81
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-06 09:22:52 +0300

    header updated

--- src/components/header.js
+++ src/components/header.js
@@ -24,23 +24,23 @@ function Header() {
           <div className="header__brand">
             <h1 className="header__brand-name">Kashgames</h1>
           </div>
-          
-          <nav className="header__nav">
-            <ul className="header__nav-menu">
-              <li className="header__nav-item">
-                <NavLink exact to="/" className="header__nav-link" activeClassName="active">All Games</NavLink>
-              </li>
-              {categories.map((category) => (
-                <li className="header__nav-item" key={category}>
-                  <NavLink to={`/category/${category}`} className="header__nav-link" activeClassName="active">{category}</NavLink>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          
-          
         </div>
       </header>
+
+      <nav className="header__nav">
+        <div className="container">
+          <ul className="header__nav-menu">
+            <li className="header__nav-item">
+              <NavLink exact to="/" className="header__nav-link" activeClassName="active">All Games</NavLink>
+            </li>
+            {categories.map((category) => (
+              <li className="header__nav-item" key={category}>
+                <NavLink to={`/category/${category}`} className="header__nav-link" activeClassName="active">{category}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
 
       {category ? <Category /> : null}
     </>
