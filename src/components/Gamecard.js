307fd4d4e8abdf8ceb207f66a95a3b0025326675
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-11 10:43:57 +0300

    pages updated

--- src/components/Gamecard.js
+++ src/components/Gamecard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Styles/Gamecard.css';
 import GameDetails from './Gamedetails';
 
-function Gamecard({ title, image, description }) {
+function Gamecard({ id, title, image, description }) {
   const [showModal, setShowModal] = useState(false);
 
   const handlePlayNow = () => {
@@ -15,11 +15,12 @@ function Gamecard({ title, image, description }) {
 
   return (
     <div className="gamecard">
+    
       <img src={image} alt={title} className="gamecard__image" />
       <div className="gamecard__content">
         <h3 className="gamecard__title">{title}</h3>
         <button className="gamecard__button" onClick={handlePlayNow}>Play Now</button>
-        {showModal && <GameDetails title={title} image={image} description={description}  onClose={handleClose} />}
+        {showModal && <GameDetails id={id} title={title} image={image} description={description}  onClose={handleClose} />}
       </div>
     </div>
   );
