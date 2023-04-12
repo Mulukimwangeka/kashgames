2493e5fc510b0752a239c07fa19200337b7b424a
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-12 08:59:27 +0300

    updated css

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ import Category from './category';
 function Header() {
   const [categories, setCategories] = useState([]);
   const { category } = useParams();
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -17,6 +18,10 @@ function Header() {
     fetchCategories();
   }, []);
 
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
   return (
     <>
       <header className="header">
@@ -24,6 +29,12 @@ function Header() {
           <div className="header__brand">
             <h1 className="header__brand-name">KashGames</h1>
           </div>
+          <button
+            className="header__mobile-menu-btn"
+            onClick={toggleMobileMenu}
+          >
+            {showMobileMenu ? 'Close' : 'Menu'}
+          </button>
         </div>
       </header>
 
@@ -31,16 +42,46 @@ function Header() {
         <div className="container">
           <ul className="header__nav-menu">
             <li className="header__nav-item">
-              <NavLink exact to="/" className="header__nav-link" activeclassname="active">All Games</NavLink>
+              <NavLink exact to="/" className="header__nav-link" activeClassName="active">All Games</NavLink>
             </li>
             {categories.map((category) => (
               <li className="header__nav-item" key={category}>
-                <NavLink to={`/category/${category}`} className="header__nav-link" activeclassname="active">{category}</NavLink>
+                <NavLink to={`/category/${category}`} className="header__nav-link" activeClassName="active">{category}</NavLink>
               </li>
             ))}
           </ul>
         </div>
       </nav>
+
+      {showMobileMenu && (
+        <nav className="header__mobile-nav">
+          <ul className="header__mobile-nav-menu">
+            <li className="header__mobile-nav-item">
+              <NavLink
+                exact
+                to="/"
+                className="header__mobile-nav-link"
+                activeClassName="active"
+                onClick={toggleMobileMenu}
+              >
+                All Games
+              </NavLink>
+            </li>
+            {categories.map((category) => (
+              <li className="header__mobile-nav-item" key={category}>
+                <NavLink
+                  to={`/category/${category}`}
+                  className="header__mobile-nav-link"
+                  activeClassName="active"
+                  onClick={toggleMobileMenu}
+                >
+                  {category}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
 
       {category ? <Category /> : null}
     </>
