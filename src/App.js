abddaef240839234b4f8801a1c390cae0e02f7de
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-19 12:04:05 +0300

    phone games detail modal fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Category from './components/category';
-import Filter from './components/filter';
 // import Gamecard from './components/Gamecard';
 import Header from './components/header';
 import Gamedetails from './components/Gamedetails';
 import Gamelist from './components/Gamelist';
-import Subscription from './components/Subscription';
-import Search from './components/search';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -17,12 +15,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Gamelist />} />
         <Route path="/game/:id" element={<Gamedetails />} />
-        <Route path="/subscription" element={<Subscription />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/filter" element={<Filter />} />
         <Route path="/category/:categoryId" element={<Category/>} />
-
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Kashgames © 2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
